2140a32b12d9d808a08e1de9e7488da4ceaea310
MinhLong <leminhlong11b8@gmail.com>
2026-07-20 22:25:22 +0700

    Initial Laravel project

---
+++ database/migrations/2026_07_20_150802_create_bookings_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('room_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->string('guest_name');
+
+            $table->string('guest_phone');
+
+            $table->date('check_in');
+
+            $table->date('check_out');
+
+            $table->enum('status', [
+                'pending',
+                'confirmed',
+                'checked_in',
+                'checked_out',
+                'cancelled'
+            ])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
