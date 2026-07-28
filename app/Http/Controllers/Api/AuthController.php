b565a750974cb0f598b3f6badf41db44ae14d9a9
MinhLong <leminhlong11b8@gmail.com>
2026-07-29 00:26:44 +0700

    Implement authentication features in AuthController and AuthService, including user registration, login, logout, and profile retrieval

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,58 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
+use App\Services\AuthService;
+use App\Traits\ApiResponse;
 
 class AuthController extends Controller
 {
-    //
+    use ApiResponse;
+    public function __construct(
+        private readonly AuthService $authService
+    ) {}
+
+    public function register(RegisterRequest $request)
+    {
+        $result = $this->authService->register($request->validated());
+
+        return $this->success([
+            'user' => new UserResource($result['user']),
+            'token' => $result['token'],
+        ], 'Register successfully.', 201);
+    }
+
+
+    public function login(LoginRequest $request)
+    {
+        $result = $this->authService->login($request->validated());
+
+        return $this->success([
+            'user' => new UserResource($result['user']),
+            'token' => $result['token'],
+        ], 'Login successfully.');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->authService->logout($request->user());
+
+        return $this->success(
+            null,
+            'Logout successfully.'
+        );
+    }
+
+    public function me(Request $request)
+    {
+        return $this->success(
+            new UserResource(
+                $this->authService->me($request->user())
+            ),
+            'User profile retrieved successfully.'
+        );
+    }
 }
